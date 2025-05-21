b749ed8d178820a45c06da7419a1d7bc69db3546
Alexander Marynchev <alemar27@bergen.org>
2025-05-21 09:40:55 -0400

    adds bitboard and bit manipulation and an updated makefile

---
+++ bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bit.h"
+
+//set/get/swap macros
+void printBitboard(U64 bitboard) {
+    for(int i = 0; i < 64; i++) {
+
+        //print ranks
+        if (i % 8 == 0) {
+            printf("\n%d    ", 8 - (i / 8));
+        }
+
+        //print bit
+        printf("%d ", GET_BIT(bitboard, i));
+    }
+    printf("\n\n     a b c d e f g h\n");
+
+    //print bitboard as unsigned decimal
+    printf("\n      Bitboard: %llud\n", bitboard);
+}
--- cm8.c
+++ cm8.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#include "bit.h"
+
 int main() {
-    printf("cm8 engine \n");
+    printf("cm8 engine\n");
+
+    U64 bitboard = 0ULL;  // Example bitboard
+    printBitboard(bitboard);
+    SET_BIT(bitboard, a8);
+    printBitboard(bitboard);
+    printf("Bit at a8: %d\n", GET_BIT(bitboard, a8));
     return 0;
 }
