ef15054bc3527753195e953c5cc590e3d6d0f6be
MaxN1322 <maxner27@bergen.org>
2025-06-02 13:06:42 -0400

    Just messing with the JNI, I'm trying to make this run entirely in vs code so that my groupmates could run it on their computers.

--- src/testFile.java
+++ src/testFile.java
@@ -6,8 +6,13 @@ public class testFile {
     }
 
     public static void main(String[] args) {
+        try{
         testFile t = new testFile();
         long result = t.maskBishopAttacks(28); // from what i understand e4 is square 28
         System.out.println("Bitboard result: " + result);
+        } catch (UnsatisfiedLinkError e) {
+            System.out.println("Native code library failed to load.\n" + e);
+        }
+        }
     }
 }
