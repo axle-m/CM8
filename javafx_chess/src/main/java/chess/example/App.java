1bb68e97f07ccca583cf77993c89014aa311d04d
gabmoj27 <gmojicaschools@gmail.com>
2025-06-02 15:21:27 -0400

    Javafx File
    Created javafx file so you don't have to download it and can import javafx things

---
+++ javafx_chess/src/main/java/chess/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+//C:\Users\gmoji\OneDrive\Documents\GitHub\CM8\javafx_chess\src
