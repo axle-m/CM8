a351ebe38206e068de247aa5512706cb1b0cd96c
Alexander Marynchev <alemar27@bergen.org>
2025-06-07 19:27:46 -0400

    forgot to stage changes

--- src/cm8.c
+++ src/cm8.c
@@ -6,16 +6,6 @@
 
 #include "driver.h"
 
-
-uint64 unix_time_ms() {
-    FILETIME ft;
-    GetSystemTimeAsFileTime(&ft);
-    uint64 t = ((uint64)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
-    // FILETIME is in 100-nanosecond intervals since Jan 1, 1601
-    // Convert to milliseconds since Unix epoch (Jan 1, 1970)
-    return (t - 116444736000000000ULL) / 10000ULL;
-}
-
 int main(int argc, char *argv[]) {
     printf("cm8 engine\n");
     init();
