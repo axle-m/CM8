48e27a9d76993512bb3d4f6cb155cc3cc8037ac8
Alexander Marynchev <alemar27@bergen.org>
2025-06-07 18:34:38 -0400

    working fen parsing

--- src/cm8.c
+++ src/cm8.c
@@ -7,8 +7,22 @@
 int main(int argc, char *argv[]) {
     printf("cm8 engine\n");
     init();
-    
+
+    parseFen("rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w Kkq a4 0 1");
     printBoard();
+
+    parseFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+    printBoard();
+
+    parseFen("r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9");
+    printBoard();
+
+    parseFen("8/8/8/8/8/8/8/8 w - - 0 1");
+    printBoard();
+
+    parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    printBoard();
+
 
     cleanup();
     return 0;
--- src/inputProcessor.c
+++ src/inputProcessor.c
@@ -2,8 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 
-int squareToPos(char *square) {
+#include "bit.h"
+
+int squareToPos(char *square) {\
+    printf("%c%c\n", square[0], square[1]);
     int file = square[0] - 'a';
-    int rank = 7 - (square[1] - '1');
+    int rank = 8 - (square[1] - '0');
+    printf("File: %d, Rank: %d\n", file, rank);
     return rank * 8 + file;
+}
+
+void parseFen(char *fen){
+    //reset everything
+    memset(bitboards, 0ULL, sizeof(bitboards));
+    memset(occupancies, 0ULL, sizeof(occupancies));
+    side = white;
+    enpassant = no_sq;
+    castle = 0;
+
+    //piece placement
+    int rank = 0, file = 0;
+    while(*fen != ' ') {
+        char c = *fen;
+        *fen++;
+        if(c == '/') {
+            rank++;
+            file = 0;
+        } else if(c >= '1' && c <= '8') {
+            file += c - '0';
+        } else {
+            int piece;
+            switch(c) {
+                case 'P': piece = P; break;
+                case 'N': piece = N; break;
+                case 'B': piece = B; break;
+                case 'R': piece = R; break;
+                case 'Q': piece = Q; break;
+                case 'K': piece = K; break;
+                case 'p': piece = p; break;
+                case 'n': piece = n; break;
+                case 'b': piece = b; break;
+                case 'r': piece = r; break;
+                case 'q': piece = q; break;
+                case 'k': piece = k; break;
+                default: continue;
+            }
+            int sq = rank * 8 + file;
+            SET_BIT(bitboards[piece], sq);
+            file++;
+        }
+    }
+    *fen++;
+
+    //init occupancy bitboards
+    for(int piece = P; piece <= K; piece++) occupancies[white] |= bitboards[piece];
+    for(int piece = p; piece <= k; piece++) occupancies[black] |= bitboards[piece];
+    occupancies[both] = occupancies[white] | occupancies[black];
+
+    //side to move
+    side = (*fen == 'w') ? white : black;
+    *fen++;
+    *fen++;
+
+    //castling rights
+    while(*fen != ' ') {
+        switch(*fen) {
+            case 'K': castle |= WK; break;
+            case 'Q': castle |= WQ; break;
+            case 'k': castle |= BK; break;
+            case 'q': castle |= BQ; break;
+            case '-': break;
+        }
+        *fen++;
+    }
+    *fen++;
+
+    //en passant
+    enpassant = (*fen == '-') ? no_sq : squareToPos(fen);
+    while(*fen != ' ' && *fen != '\0') *fen++;
+    *fen++;
+
+//     //halfmove clock
+//     while(*fen != ' ') *fen++;
+//     *fen++;
+
+//     //fullmove number
+//     while(*fen != ' ' && *fen != '\0') *fen++;
 }
