edc73c22f661844d365cb54c5f4ae0fe1a7f413b
gabmoj27 <gmojicaschools@gmail.com>
2025-06-10 13:21:05 -0400

    More GUI work
    there are some very apparrant issues with it however the set up is there and once i fix those we can then connect the bot with this and get stuff working=

--- javafx_chess/src/main/java/chess/example/Challege.java
+++ javafx_chess/src/main/java/chess/example/Challege.java
@@ -1,54 +1,91 @@
 package chess.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 import java.io.FileInputStream;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventType;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
-public class Challege extends Application{
-    private Stage stage;
+public class Challege extends Application {
+    private static final int SIZE = 8;
+    private ChessSquare[][] squares = new ChessSquare[SIZE][SIZE];
+    private String[][] board = new String[SIZE][SIZE]; // piece codes like "wp", "bk", etc.
+    private int selectedRow = -1, selectedCol = -1;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {      
-        FileInputStream inputstream = new FileInputStream("assests/bb.png");        
-        Image image = new Image(inputstream);
-        ImageView imageView = new ImageView(image); 
+    public void start(Stage primaryStage) throws Exception {
+        GridPane grid = new GridPane();
 
-        this.stage = primaryStage;
+        // Example: Set up a few pieces for demo. Expand to full setup as needed.
+        board[1][4] = "bp"; // black pawn
+        board[6][4] = "wp"; // white pawn
+        board[0][3] = "bq"; // black queen
+        board[7][3] = "wq"; // white queen
 
-        imageView.setX(100); 
-        imageView.setY(200);
-        
-        //setting the fit height and width of the image view 
-        imageView.setFitHeight(200); 
-        imageView.setFitWidth(200); 
-        
-        //Setting the preserve ratio of the image view 
-        imageView.setPreserveRatio(true);  
-        
-        //Creating a Group object  
-        Group root = new Group(imageView);  
-        
-        //Creating a scene object 
-        Scene scene = new Scene(root, 600, 500);  
-        
-        //Setting title to the Stage 
-        primaryStage.setTitle("Loading an image");  
-        
-        //Adding scene to the stage 
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                String pos = "" + (char)('a' + col) + (8 - row);
+                String piece = board[row][col];
+                Image img = null;
+                if (piece != null) {
+                    img = new Image(new FileInputStream("assests/" + piece + ".png"));
+                }
+                ChessSquare square = new ChessSquare(pos, piece, img);
+                square.setPrefSize(60, 60);
+                square.setStyle("-fx-background-color: " + ((row + col) % 2 == 0 ? "#f0d9b5" : "#b58863") + ";");
+                int r = row, c = col;
+                square.setOnAction(e -> handleClick(r, c));
+                squares[row][col] = square;
+                grid.add(square, col, row);
+            }
+        }
+
+        Scene scene = new Scene(grid, 480, 480);
+        primaryStage.setTitle("Chess GUI");
         primaryStage.setScene(scene);
-        
-        //Displaying the contents of the stage 
-        primaryStage.show(); 
+        primaryStage.show();
     }
-      public static void main(String args[]) { 
-      launch(args); 
-   } 
+
+    private void handleClick(int row, int col) {
+        ChessSquare clicked = squares[row][col];
+        if (selectedRow == -1 && clicked.getPiece() != null) {
+            // Select a piece
+            selectedRow = row;
+            selectedCol = col;
+            clicked.setStyle(clicked.getStyle() + "-fx-border-color: red; -fx-border-width: 3;");
+        } else if (selectedRow != -1) {
+            // Attempt move
+            if (selectedRow != row || selectedCol != col) {
+                // Move the piece
+                String movingPiece = board[selectedRow][selectedCol];
+                board[row][col] = movingPiece;
+                board[selectedRow][selectedCol] = null;
+                updateSquare(row, col, movingPiece);
+                updateSquare(selectedRow, selectedCol, null);
+                System.out.println("Moved " + movingPiece + " from " + squares[selectedRow][selectedCol].getPosition() + " to " + squares[row][col].getPosition());
+            }
+            // Reset selection
+            squares[selectedRow][selectedCol].setStyle("-fx-background-color: " + ((selectedRow + selectedCol) % 2 == 0 ? "#f0d9b5" : "#b58863") + ";");
+            selectedRow = selectedCol = -1;
+        }
+    }
+
+    private void updateSquare(int row, int col, String piece) {
+        board[row][col] = piece;
+        Image img = null;
+        if (piece != null) {
+            try {
+                img = new Image(new FileInputStream("assests/" + piece + ".png"));
+            } catch (Exception e) {
+                img = null;
+            }
+        }
+        squares[row][col].setPiece(piece, img);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
